137f00bd979b6f96c66eb28206c2d65854504fa2
Kandukoori Anudeep <kandukoorianudeep@gmail.com>
2026-03-03 21:02:22 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,9 +1,15 @@
 from turtle import Turtle
+import os
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+
+        #Here,  we the below because we vs code cannot directly use it from the main game file so use this path
+        file_path = os.path.join(os.path.dirname(__file__), "data.txt")
+        with open(file_path) as hs:
+            self.highscore = int(hs.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -13,8 +19,14 @@ class Scoreboard(Turtle):
     def increase_score(self):
         self.clear()
         self.score+=1
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 22, "normal"))
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align="center", font=("Arial", 22, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align="center", font=("Arial", 22, "normal"))
+    def reset(self):
+        if(self.score>self.highscore):
+            self.highscore = self.score
+            #Here, it will write the high score of the game in the file to a new high score
+            file_path = os.path.join(os.path.dirname(__file__), "data.txt")
+            with open(file_path, mode='w') as hs:
+                hs.write(f"{self.highscore}")
+        self.score = 0
+        self.increase_score()
